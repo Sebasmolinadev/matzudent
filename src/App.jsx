3e54986e547923147cd5be539f9b6d7e25ea321d
sebasmol <yosoysebasm@gmail.com>
2024-09-04 23:03:47 -0500

    primer avance

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import isologo from './img/logo.png';
 
 function App() {
-  
+  return (
+    <div className='nav'>
+      <div className='isolg'>
+        <img src={isologo} alt="Matzu"/>
+      </div>
+
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="#home" className="nav-link">Acerca de nosotros</a>
+        </li>
+        <li className="nav-item">
+          <a href="#about" className="nav-link">Centro de conocimientos</a>
+        </li>
+        <li className="nav-item">
+          <a href="#services" className="nav-link">Testimonios</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact" className="nav-link">Contactanos</a>
+        </li>
+      </ul>
+
+
+    </div>
+
+  )
+
 }
 
 export default App
